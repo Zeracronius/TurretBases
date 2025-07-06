c5c12efdcb697f170427e08d33bb01e86fc85d34
Zeracronius <zeracronius@outlook.dk>
2025-07-06 19:53:09 +0200

    Added mod settings

--- Source/TurretBases/Mod/TurretBasesMod.cs
+++ Source/TurretBases/Mod/TurretBasesMod.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TurretBases.UserInterface;
+using UnityEngine;
 using Verse;
 
 namespace TurretBases.Mod
 {
 	internal class TurretBasesMod : Verse.Mod
 	{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+		public static TurretBasesSettings Settings { get; private set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+		private FilterTreeBox? _treeBox;
+
 		public TurretBasesMod(ModContentPack content) 
 			: base(content)
 		{
+			Settings = this.GetSettings<TurretBasesSettings>();
+		}
+
+
+		public override string SettingsCategory()
+		{
+			return "Turret bases";
+		}
+
+		public override void DoSettingsWindowContents(Rect inRect)
+		{
+			if (_treeBox == null)
+				_treeBox = new FilterTreeBox(Settings.GetSettingsNodes());
+
+			_treeBox.Draw(inRect);
 		}
 	}
 }
---
+++ Source/TurretBases/Mod/TurretBasesSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurretBases.UserInterface.TreeBox;
+using UnityEngine;
+using Verse;
+
+namespace TurretBases.Mod
+{
+	public class TurretBasesSettings : ModSettings
+	{
+		public bool WeaponsDegrade = true;
+		public bool WeaponsCanBreak = true;
+		public uint ShotsToBreakWeapon = 1000;
+
+		internal List<TreeNode_FilterBox> GetSettingsNodes()
+		{
+			var degradationNode = new TreeNode_FilterBox("Degredation", "Weapons degrades when shot by turret platform.",
+					(in Rect rect) => Widgets.Checkbox(rect.position, ref WeaponsDegrade));
+
+			degradationNode.AddChild("Weapon can break", "If enabled, mounted guns will keep shooting until they break, otherwise it will stop firing when too damaged.",
+					(in Rect rect) => Widgets.Checkbox(rect.position, ref WeaponsCanBreak));
+
+			degradationNode.AddChild("Bursts to destroy", "Number of bursts a weapon will last before breaking.",
+					(in Rect rect) => ShotsToBreakWeapon = (uint)Widgets.HorizontalSlider(rect, ShotsToBreakWeapon, 100f, 10000f, 
+						label: ShotsToBreakWeapon.ToString(), leftAlignedLabel: "100", rightAlignedLabel: "10000", roundTo: 1), splitRow: true);
+
+
+			List<TreeNode_FilterBox> nodes = new List<TreeNode_FilterBox>
+			{
+				degradationNode
+			};
+			return nodes;
+		}
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+		}
+	}
+}
